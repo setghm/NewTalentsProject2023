48cde4202cc9fc66138472d7397edfb5aeb876a5
Set HM <53455269+Usuario256@users.noreply.github.com>
2023-10-30 23:25:09 -0600

    Exposición

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,28 +1,38 @@
 #include <WiFi.h>
 #include "ClienteThingSpeak.h"
 
-#define WIFI_SSID "UPQ Alumnos"
-#define WIFI_PWD  ""
+#define WIFI_SSID "HMWIFI"
+#define WIFI_PWD  "vivianita"
 
 #define THINGSPEAK_CHANNEL  2293246
 #define THINGSPEAK_APIKEY   "GJ1O4PWAOGMR7SD2"
 
-#define FIELD_VOLTAJE_CPU 1
-#define FIELD_CORRIENTE_CPU 2
-#define FIELD_VOLTAJE_GPU 3
-#define FIELD_CORRIENTE_GPU 4
-#define FIELD_VOLTAJE_TPU 5
-#define FIELD_CORRIENTE_TPU 6
+#define FIELD_ENCENDIDO 1
+#define FIELD_POTENCIA 2
+#define FIELD_CORRIENTE 3
+
+#define DEFAULT_DELAY 3000
 
 void setup() {
+  // Configurar el LED builtin para indicar si se ha conectado o no a internet
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  
   // Conectarse a internet
   WiFi.begin(WIFI_SSID, WIFI_PWD);
   while (WiFi.status() != WL_CONNECTED);
+
+  // Indicar que se ha conectado a internet
+  digitalWrite(2, HIGH);
+  
   // No iniciar el cliente de thingspeak hasta que haya una conexión a internet existente
   ClienteThingSpeak.begin(THINGSPEAK_CHANNEL, THINGSPEAK_APIKEY);
 }
 
 void loop() {
   // Ejemplo
-  ClienteThigSpeak.guardarDatos(FIELD_VOLTAJE_CPU, 115);
+  ClienteThingSpeak.guardarDatos(FIELD_POTENCIA, 1);
+  ClienteThingSpeak.guardarDatos(FIELD_CORRIENTE, 1);
+
+  delay(DEFAULT_DELAY);
 }
