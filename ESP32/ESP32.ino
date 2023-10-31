5a0ea1e55cf0f92994d30891b8448f9dda53821c
Set HM <53455269+Usuario256@users.noreply.github.com>
2023-10-30 23:41:18 -0600

    Update ESP32.ino

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,8 +1,8 @@
 #include <WiFi.h>
 #include "ClienteThingSpeak.h"
 
-#define WIFI_SSID "HMWIFI"
-#define WIFI_PWD  "vivianita"
+#define WIFI_SSID "UPQ-Alumnos"
+#define WIFI_PWD  ""
 
 #define THINGSPEAK_CHANNEL  2293246
 #define THINGSPEAK_APIKEY   "GJ1O4PWAOGMR7SD2"
@@ -12,6 +12,10 @@
 #define FIELD_CORRIENTE 3
 
 #define DEFAULT_DELAY 3000
+
+int potencia, corriente;
+
+void calcularConsumo();
 
 void setup() {
   // Configurar el LED builtin para indicar si se ha conectado o no a internet
@@ -30,9 +34,19 @@ void setup() {
 }
 
 void loop() {
-  // Ejemplo
+  calcularConsumo();
+
+  // Guardar datos
   ClienteThingSpeak.guardarDatos(FIELD_POTENCIA, 1);
   ClienteThingSpeak.guardarDatos(FIELD_CORRIENTE, 1);
 
   delay(DEFAULT_DELAY);
+}
+
+void calcularConsumo() {
+
+  
+
+  corriente = 0;
+  potencia = 0;
 }
